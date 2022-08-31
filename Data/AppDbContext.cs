b6699aa09b3070b451ed9fad3ebb4234fc246c5f
Larissa <lrstvrs@gmail.com>
2022-08-31 08:05:59 -0300

    Adiciona codigos iniciais

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MeuTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        //É o contexto de dados da aplicação
+        //É a representação do banco de memória
+
+
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace MeuTodo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
